9ec48a31cba48a5bfe0f6e519f5ec39e84362d41
Jonathan Devine <dev@jonnydevine.com>
2020-07-20 02:45:40 +0100

    Created

---
+++ src/EventListener/AddJoinedActivityListener.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Event\UserEvent;
+use App\Event\UserEvents;
+use App\Service\ActivityService;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class AddJoinedActivityListener implements EventSubscriberInterface
+{
+    /**
+     * @var ActivityService
+     */
+    private $activityService;
+
+    /**
+     * @param ActivityService $activityService
+     */
+    public function __construct(
+        ActivityService $activityService
+    )
+    {
+        $this->activityService = $activityService;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            UserEvents::USER_CONFIRMED => "onUserConfirmed"
+        );
+    }
+
+    /**
+     * @param UserEvent $userEvent
+     */
+    public function onUserConfirmed(UserEvent $userEvent)
+    {
+        $user = $userEvent->getUser();
+        $this->activityService->addJoinedActivity($user);
+    }
+}
---
+++ src/EventListener/AddWatchlistActivityListener.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\EventListener;
+
+use App\Event\WatchlistEvent;
+use App\Event\WatchlistEvents;
+use App\Service\ActivityService;
+use App\Service\DocumentaryService;
+use App\Service\UserService;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+
+class AddWatchlistActivityListener implements EventSubscriberInterface
+{
+    /**
+     * @var ActivityService
+     */
+    private $activityService;
+
+    /**
+     * @param ActivityService $activityService
+     */
+    public function __construct(
+        ActivityService $activityService
+    )
+    {
+        $this->activityService = $activityService;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getSubscribedEvents()
+    {
+        return array(
+            WatchlistEvents::WATCHLIST_CREATED => "onWatchlistCreated"
+        );
+    }
+
+    /**
+     * @param WatchlistEvent $watchlistEvent
+     * @throws \Doctrine\ORM\ORMException
+     */
+    public function onWatchlistCreated(WatchlistEvent $watchlistEvent)
+    {
+        $watchlist = $watchlistEvent->getWatchlist();
+        $this->activityService->addWatchlistActivity($watchlist);
+    }
+}
